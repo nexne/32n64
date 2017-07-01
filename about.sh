76582008c555e09727c4d54eaa9376fa7f60c80a
nexne <nexne@unpak.ac.id>
2017-07-01 21:38:58 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#About this script
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+echo -e ""
+echo -e "vps"
+echo -e "===================================================="
+echo -e "Script Auto Install SSH" 
+echo -e "===================================================="
+echo -e ""
+echo -e "For Debian 7 32 bit"
+echo -e "VPS DEBIAN"
+echo -e ""
+echo -e "Original script  :"
+echo -e "* Siapa?"
+echo -e "* "
+echo -e "* "
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e "Facebook : https://www.facebook.com/"
+echo -e "WhatsApp       : 0"
+echo -e "Website        : http://"
+echo -e "Website        : http://"
+echo -e "Saran"
+
+echo -e ""
