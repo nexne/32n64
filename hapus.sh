cf1664795074a153362baaf713f8e5c160dc1cce
nexne <nexne@unpak.ac.id>
2017-07-02 17:02:48 +0800

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-echo "-------------------------------"
-echo "USERNAME          EXP DATE     "
-echo "-------------------------------"
-while read expired
-do
-        AKUN="$(echo $expired | cut -d: -f1)"
-        ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
-        if [[ $ID -ge 1000 ]]; then
-        printf "%-17s %2s\n" "$AKUN" "$exp"
-        fi
-done < /etc/passwd
-JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo "-------------------------------"
-echo "Jumlah akun: $JUMLAH user"
-echo "-------------------------------"
+#Script untuk menghapus user
 
-echo -e ""
-read -p "Nama user SSH yang akan dihapus : " Nama
+read -p "Hapus User : " Pengguna
 
-userdel -r $Nama
-
-echo -e "ALL SUPPORTED by Yonatan Kanu as THORN SSH"
-echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
-echo -e "WhatsApp       : 085707136028"
-echo -e "Website        : http://yonatankanu.blogspot.co.id"
-echo -e "Website        : http://yonatankanu.wordpress.com/"
-
-echo -e ""
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna telah dihapus."
+else
+        echo -e "GAGAL: User $Pengguna tidak ada."
+fi
