f4f16eddc146891c76d9cfd5a0b8517180543f75
nexne <nexne@unpak.ac.id>
2017-07-08 00:15:48 -0700

    Update resvis.sh

--- resvis.sh
+++ resvis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Restart service dropbear, webmin, squid3, openvpn, openssh
-# Created by Yonatan Kanu as THORN SSH
+# Created 
 service dropbear restart
 service webmin restart
 service squid3 restart
