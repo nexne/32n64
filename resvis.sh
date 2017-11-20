e2de4edc8f843fbd620cdefce8283431b6b56152
nexne <nexne@unpak.ac.id>
2017-11-20 21:30:59 +0800

    Update resvis.sh

--- resvis.sh
+++ resvis.sh
@@ -2,7 +2,7 @@
 # Restart service dropbear, webmin, squid3, openvpn, openssh
 # Created 
 service dropbear restart
-service webmin restart
+#service webmin restart
 service squid3 restart
 service openvpn restart
 service ssh restart
